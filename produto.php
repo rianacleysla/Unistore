9341d338dbb25e9e56d8fed082fb7422be7fc55e
ana <jacintojogo@gmail.com>
2026-08-11 11:33:36 -0300

    Update produto.php

--- produto.php
+++ produto.php
@@ -49,7 +49,7 @@
     <div class="produto-card">
         <div class="produto-imagem"><img src="https://i.pinimg.com/736x/cb/33/9a/cb339a683738c3e97a95f37d377d69f3.jpg" alt="MLP"></div>
         <div class="produto-info">
-            <p class="produto-nome">Item 1</p>
+            <p class="produto-nome">Miniaturas My Little Pony/p>
             <p class="produto-preco">$5</p>
         </div>
     </div>
@@ -57,7 +57,7 @@
     <div class="produto-card">
         <div class="produto-imagem"><img src="https://i.pinimg.com/736x/f5/0f/01/f50f017ea579e3c8a544f052aa400a1b.jpg" alt="Unicornio"></div>
         <div class="produto-info">
-            <p class="produto-nome">Item 2</p>
+            <p class="produto-nome">Unicornio de pelucia</p>
             <p class="produto-preco">$10</p>
         </div>
     </div>
@@ -65,16 +65,16 @@
     <div class="produto-card">
         <div class="produto-imagem"><img src="https://i.pinimg.com/736x/07/a2/e2/07a2e259bac5f66945a1a75832d86ee1.jpg" alt="Ponei"></div>
         <div class="produto-info">
-            <p class="produto-nome">Item 3</p>
-            <p class="produto-preco">$3</p>
+            <p class="produto-nome">Unicornios de porcelana/p>
+            <p class="produto-preco">$13</p>
         </div>
     </div>
 
     <div class="produto-card destaque">
         <div class="produto-imagem"><img src="https://i.pinimg.com/736x/16/9e/6e/169e6e07391edec0c579f7c968333bfd.jpg" alt="ble"></div>
         <div class="produto-info">
-            <p class="produto-nome">Featured Item</p>
-            <p class="produto-preco">$0</p>
+            <p class="produto-nome">Carrossel/p>
+            <p class="produto-preco">$20</p>
         </div>
     </div>
 
