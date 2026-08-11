0180c8003f054645cabc032fac220a50eb51bcb4
ana <jacintojogo@gmail.com>
2026-08-11 11:18:51 -0300

    Add initial HTML structure and styles in style.css

---
+++ style.css
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Unistore</title>
+
+<!-- FONTES -->
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@500;700&family=Poppins:wght@300;400;500&display=swap" rel="stylesheet">
+
+<!-- CSS EXTERNO -->
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<header>
+
+    <div class="logo">
+        <img src="https://media.tenor.com/OoG1CF2T3QIAAAAi/kucing-scuba-scuba-cat.gif" alt="Logo" style="width: 60px; height: 60px; object-fit: contain;">
+    </div>
+    <nav>
+        <a href="#">Produtos</a>
+        <a href="#">Categorias</a>
+    </nav>
+
+    <div class="buttons">
+
+        <a href="login_cliente.php" class="btn btn-login">Cliente</a>
+        <a href="login_vendedor.php" class="btn btn-login">Vendedor</a>
+        <a href="cadastro.php" class="btn btn-register">Criar Conta</a>
+
+    </div>
+
+</header>
+
+<div class="sort-bar">
+    <button class="sort-btn ativo">Novo</button>
+    <button class="sort-btn">Maior preço</button>
+    <button class="sort-btn">Menor preço</button>
+</div>
+
+<div class="produto-grid">
+
+    <div class="produto-card">
+        <div class="produto-imagem"><img src="https://i.pinimg.com/736x/cb/33/9a/cb339a683738c3e97a95f37d377d69f3.jpg" alt="MLP"></div>
+        <div class="produto-info">
+            <p class="produto-nome">Item 1</p>
+            <p class="produto-preco">$5</p>
+        </div>
+    </div>
+
+    <div class="produto-card">
+        <div class="produto-imagem"><img src="https://i.pinimg.com/736x/f5/0f/01/f50f017ea579e3c8a544f052aa400a1b.jpg" alt="Unicornio"></div>
+        <div class="produto-info">
+            <p class="produto-nome">Item 2</p>
+            <p class="produto-preco">$10</p>
+        </div>
+    </div>
+
+    <div class="produto-card">
+        <div class="produto-imagem"><img src="https://i.pinimg.com/736x/07/a2/e2/07a2e259bac5f66945a1a75832d86ee1.jpg" alt="Ponei"></div>
+        <div class="produto-info">
+            <p class="produto-nome">Item 3</p>
+            <p class="produto-preco">$3</p>
+        </div>
+    </div>
+
+    <div class="produto-card destaque">
+        <div class="produto-imagem"><img src="https://i.pinimg.com/736x/16/9e/6e/169e6e07391edec0c579f7c968333bfd.jpg" alt="ble"></div>
+        <div class="produto-info">
+            <p class="produto-nome">Featured Item</p>
+            <p class="produto-preco">$0</p>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
